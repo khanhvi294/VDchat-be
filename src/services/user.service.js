bb0fa11826ca040762009d8fbd54d53a442d8592
Vi Nguyen <khanhvi2941@gmail.com>
2023-04-23 16:42:44 +0700

    update api user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,28 +1,13 @@
 import UserModel from "../models/user.model";
 
-const getUserInfo = (userId) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let user = await UserModel.findById(userId, { providers: 0, role: 0 });
-      resolve(user);
-    } catch (error) {
-      reject({
-        success: false,
-        error: error,
-      });
-    }
-  });
+const getUserInfo = async (userId) => {
+  return await UserModel.findById(userId, { providers: 0, role: 0 });
 };
 
 const updateUserInfo = async (userId, data) => {
-  try {
-    let userUpdate = await UserModel.findByIdAndUpdate(userId, data, {
-      returnDocument: "after",
-    });
-    return userUpdate;
-  } catch (error) {
-    return error;
-  }
+  return await UserModel.findByIdAndUpdate(userId, data, {
+    returnDocument: "after",
+  });
 };
 
 const blockUser = async (userId, blockUserId) => {

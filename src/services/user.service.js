c125dca72642fde61fbac6b2d847f5073c47a201
Vi Nguyen <khanhvi2941@gmail.com>
2023-05-04 14:35:58 +0700

    feature search

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,3 +1,4 @@
+import ConversationModel from "../models/conversation.model";
 import UserModel from "../models/user.model";
 
 const getUserInfo = async (userId) => {
@@ -29,7 +30,83 @@ const unblockUser = async (userId, blockUserId) => {
     { returnDocument: "after" }
   );
 };
+const findUsersAndConversations = async (data, userId) => {
+  const conversations = await ConversationModel.find({
+    name: { $regex: data, $options: "i" },
+    participants: { $in: [userId] },
+  });
 
-const userService = { getUserInfo, updateUserInfo, blockUser, unblockUser };
+  const participantIds = conversations.reduce((acc, conversation) => {
+    return [...acc, ...conversation.participants];
+  }, []);
+
+  const filteredParticipantIds = participantIds.filter(
+    (id) => String(id) !== String(userId)
+  );
+
+  const usersWithConversation = await UserModel.find({
+    username: { $regex: data, $options: "i" },
+    _id: { $in: filteredParticipantIds },
+  });
+
+  const usersWithoutConversation = await UserModel.find({
+    username: { $regex: data, $options: "i" },
+    _id: { $nin: filteredParticipantIds, $ne: userId },
+  });
+
+  return { usersWithConversation, usersWithoutConversation, conversations };
+};
+
+//   try {
+//     const usersWithConversation = await UserModel.aggregate([
+//       {
+//         $match: {
+//           username: { $regex: data, $options: "i" },
+//           _id: { $ne: userId },
+//         },
+//       },
+//       {
+//         $lookup: {
+//           from: "conversations",
+//           localField: "conversations",
+//           foreignField: "_id",
+//           as: "conversations",
+//         },
+//       },
+//       {
+//         $match: {
+//           "conversations.participants": userId,
+//         },
+//       },
+//     ]);
+
+//     const usersWithConversationIds = usersWithConversation.map(
+//       (user) => user._id
+//     );
+
+//     const usersWithoutConversation = await UserModel.find({
+//       username: { $regex: data, $options: "i" },
+//       _id: { $ne: userId },
+//       _id: { $nin: usersWithConversationIds },
+//     });
+
+//     const conversations = await ConversationModel.find({
+//       name: { $regex: data, $options: "i" },
+//       participants: { $in: [userId] },
+//     });
+
+//     return { usersWithConversation, usersWithoutConversation, conversations };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+const userService = {
+  getUserInfo,
+  updateUserInfo,
+  blockUser,
+  unblockUser,
+  findUsersAndConversations,
+};
 
 export default userService;
